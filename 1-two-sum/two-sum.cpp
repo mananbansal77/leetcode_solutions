e81a75af1d4806b711b6c8b2b4674e8770380203
mananbansal77 <158815887+mananbansal77@users.noreply.github.com>
2025-06-16 12:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (37.24%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int, int>> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            mp.push_back({nums[i], i}); // Store value and original index
+        // sort(nums.begin(),nums.end());
+        vector<int>v;
+        vector<pair<int,int>>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+                mp.push_back({nums[i],i});
         }
-
-        sort(mp.begin(), mp.end()); // Sort by value
-
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left < right) {
-            int sum = mp[left].first + mp[right].first;
-            if (sum == target) {
-                return {mp[left].second, mp[right].second};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
+        sort(mp.begin(),mp.end());
+        int s=0;
+        int e=nums.size()-1;
+        while(s<e)
+        {
+           if(mp[s].first+mp[e].first==target)
+           {
+            v.push_back(mp[s].second);
+            v.push_back(mp[e].second);
+            break;
+           }
+           else if(mp[s].first+mp[e].first>target)
+           {
+            e--;
+           }
+           else
+           s++;
         }
+        return v;
 
-        return {}; // If no pair found
+        
     }
 };
