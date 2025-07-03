9b865b3a52d00c705b496a45d8a1aba22537bc12
Manan Bansal <158815887+mananbansal77@users.noreply.github.com>
2025-07-04 01:41:12 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,15 +4,17 @@ public:
         int min=INT_MAX;
         int profit=0;
         for(int i=0;i<prices.size();i++)
-       {
-        if(prices[i]<min)
         {
-            min=prices[i];
+            if(prices[i]<min)
+            {
+                min=prices[i];
+            }
+            else
+            {
+                profit=max(profit,prices[i]-min);
+            }
         }
-        else
-        profit=max(profit,prices[i]-min);
-
-       } 
         return profit;
+        
     }
 };
