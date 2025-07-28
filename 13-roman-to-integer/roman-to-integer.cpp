a257fb39ca61fccca307880655347aee6a86e250
Manan Bansal <158815887+mananbansal77@users.noreply.github.com>
2025-07-28 17:37:15 +0530

    Time: 4 ms (66.45%) | Memory: 13.5 MB (19.53%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int>mp={
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
+        int result=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int curr=mp[s[i]];
+            int next=(i+1<s.size())?mp[s[i+1]]:0;
+            if(curr<next)
+            {
+                result=result-curr;
+            }
+            else
+            {
+                result+=curr;
+            }
+
+        }
+        return result;
+        
+        
+    }
+};
